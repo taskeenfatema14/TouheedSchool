15b0bbbaae160196521bd7c480ed6c253520ebe9
Komal Samal <komalsamal2001@gmail.com>
2024-03-27 12:42:51 +0530

    Image field was missing added it

--- events/models.py
+++ events/models.py
@@ -7,7 +7,7 @@ from portals.base import BaseModel
 # Create your models here.
 
 class Events(BaseModel):
-    school = models.ForeignKey(School, related_name='events', on_delete=models.CASCADE)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
     event_name = models.CharField(max_length = 20)
     event_title = models.CharField(max_length = 80)
     event_date = models.DateField(null = True, blank = True)
@@ -16,7 +16,8 @@ class Events(BaseModel):
     event_desc = models.TextField(null=True, blank=True)
     thumbnail = models.ImageField(upload_to="img/thumbnails", null=True, blank=True)
     event_videos = models.FileField(upload_to='videos/', validators=[FileExtensionValidator(['mp4', 'avi', 'mov', 'wmv', 'flv'])], null=True, blank=True)
-    
+    event_images = models.ImageField(upload_to="img", default=" ", validators=[FileExtensionValidator
+            (['jpg', 'jpeg', 'png'])], null=True, blank=True)
     def _str_(self):
         return self.event_name
     
@@ -30,10 +31,10 @@ class EventImages(BaseModel):
         return f"Image for {self.event.event_name}"
     
 class EventSpeaker(BaseModel):
-    event = models.ForeignKey(Events, on_delete=models.CASCADE, ),
+    event = models.ForeignKey(Events, on_delete=models.CASCADE, null=True)
     speaker_name = models.CharField(max_length=100)
     speaker_image = models.ImageField(upload_to="speaker_images/") 
-    speaker_desc = models.TextField()
+    speaker_desc = models.TextField(null= True, blank=True)
 
     def __str__(self):
         return f"{self.speaker_name} at {self.id}"
