5d94889e7695b62e18505660cc7650c28a770f88
Komal Samal <komalsamal2001@gmail.com>
2024-04-04 10:39:53 +0530

    Added image, and speaker in EventDetailApi

--- events/urls.py
+++ events/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('events-np/',EventAPIView.as_view(), name = 'events-np'),
     path('events-np/<str:id>/',EventView.as_view(), name = 'events-np'), #Home/Event
-    path('events-details/<str:id>/',EventView.as_view(), name = 'events-details'), #Event Details(Particular School)
+    path('events-details/',EventDetails.as_view(), name = 'events-details'), #Event Details(Particular School)
     path('speaker-card/',EventSpeakersCard.as_view(), name = 'speaker-card'),
     path('speaker-card/<str:id>/',EventSpeakersCard.as_view(), name = 'speaker-card'),
 ]
