22898805f952a0de8e24cb1caef3eb1eae6eaeb6
Taskeen Fatema <fatemataskeen72@gmail.com>
2024-04-15 21:37:44 +0530

    School detail api updated

--- schools/admin.py
+++ schools/admin.py
@@ -41,11 +41,35 @@ class SchoolAdmin(admin.ModelAdmin):
                     return False
         return super().has_add_permission(request)
 
+
+# from accounts.models import User
+# from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.models import Group, Permission
+
+
+# class UserInline(admin.TabularInline):
+#     model = User
+#     extra = 0  # Control the number of empty forms displayed by default
+# class SchoolAdmin(admin.ModelAdmin):
+#     inlines = [UserInline]
+#     list_display = ('name',)
+#     search_fields = ('name',)
+
+# # Customize User admin to display only the 'school' field for school admins
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ('email', 'name', 'is_superuser', )  
+#     ordering = ('email',)  # Order by email (optional)
+#     list_filter = ('is_superuser', 'is_staff')  # Filter by these fields (optional)
+#     # ... other UserAdmin configuration options ...
+
+# admin.site.register(User, UserAdmin)
+
 # Register the SchoolAdmin class with the School model
 admin.site.register(School, SchoolAdmin)
 admin.site.register(Infrastructure)
 admin.site.register(FrequentlyAskedQuestion)
 
+# Notice Board Workinng ######################
 
 class NoticeBoardImageInline(admin.TabularInline):
     model = NoticeboardImage
@@ -57,3 +81,20 @@ class NoticeBoardAdmin(admin.ModelAdmin):
 
 admin.site.register(Noticeboard, NoticeBoardAdmin)
 
+###############################
+
+# # Get or create Django groups
+# superuser_group, created = Group.objects.get_or_create(name='Superusers')
+# school_admin_group, created = Group.objects.get_or_create(name='School Admins')
+
+# # Assign superuser permissions (modify based on your requirements)
+# superuser_group.permissions.set([
+#     # Permission to add, change, and delete all models
+#     perm for perm in Permission.objects.all()
+# ])
+
+# # Assign school admin permissions (modify based on your requirements)
+# school_admin_group.permissions.set([
+#     # Permission to add, change, and delete schools they own
+#     perm for perm in Permission.objects.filter(content_type__app_label='schools', name__in=['add_school', 'change_school', 'delete_school'])
+# ])
