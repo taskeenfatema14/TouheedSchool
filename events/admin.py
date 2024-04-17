5bd452fc817ad669885ccc6c6f834b22ea593435
Komal Samal <komalsamal2001@gmail.com>
2024-04-17 17:53:58 +0530

    Added Gallery and About us in landing page app

--- events/admin.py
+++ events/admin.py
@@ -3,25 +3,6 @@ from schools.models import *
 from events.models import *
 from .models import *
 
-# Register your models here.
-
-# @admin.register(Event)
-# class EventsAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'name', 'title', 'date', 'time', 'location']
-
-# @admin.register(EventSpeaker)
-# class EventSpeakerAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'name', 'image', 'desc']  
-
-# @admin.register(EventImages)
-# class EventImagesAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'name', 'image']
-
-#     def name(self, obj):
-#         return obj.event.name  
-    
-#     name.short_description = 'Event Name'
-
 class EventImagesInline(admin.TabularInline):
     model = EventImages
     extra = 1
