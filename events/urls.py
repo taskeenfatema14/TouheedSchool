41b836accf4198784066457b6ff3c75a8b94883c
Komal Samal <komalsamal2001@gmail.com>
2024-04-18 17:46:02 +0530

    Events through school id

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
-    path('events-list/',EventAPIView.as_view(), name = 'events-list'), #List of Events with all fields
+    # path('events-list/',EventAPIView.as_view(), name = 'events-list'), #List of Events with all fields
     path('events-list/<str:id>',EventAPIView.as_view(), name = 'events-list'), #List of Events with all fields
     path('events-np/<str:id>/',EventView.as_view(), name = 'events-np'), #Home/Event
     path('events-details/',EventDetails.as_view(), name = 'events-details'), #Event Details(Particular School)
