780faca20f3c18bbc63c057a8a34234e523bcf90
Rafat Shaikh <shaikh.raffat982@gmail.com>
2024-05-06 12:11:23 +0530

    Admissions added new field {fee concession}

--- admissions/views.py
+++ admissions/views.py
@@ -4,35 +4,8 @@ from rest_framework.views import APIView
 from django.http import Http404
 from .models import *
 from .serializers import *
-from django.core.mail import send_mail
 
-class RegisterFormView(APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = RegisterFormSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-# def send_email_to_school(student_name, email):
-#     subject = 'New Registration'
-#     message = f'Hello,\n\nA new student named {student_name} has registered.'
-#     from_email = 'komalsamalcs@gmail.com'  # Your email address
-#     recipient_list = [email]
-#     send_mail(subject, message, from_email, recipient_list)
-
-# class RegisterFormView(APIView):
-#     def post(self, request, *args, **kwargs):
-#         serializer = RegisterFormSerializer(data=request.data)
-#         if serializer.is_valid():
-#             register_form = serializer.save()
-#             student_name = serializer.data['student_name']
-#             email = register_form.school.school_email  # Assuming School model has an 'email' field
-#             send_email_to_school(student_name, email)
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-###################################Admission#############################################
-    
+# ###################################Admission#############################################
 class AdmissionsAPIView(APIView):
     def get(self, request):
         try:
@@ -53,3 +26,24 @@ class AdmissionAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            
+# class AdmissionsAPIView(APIView):
+#     def get(self, request):
+#         try:
+#             queryset = Admission.objects.all()
+#             serializer = AdmissionSerializer(queryset, many=True)
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         except Exception as e:
+#             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+# class AdmissionAPIView(APIView):
+#     def get_admissions_by_school(self, school_id):
+#         return Admission.objects.filter(school_id=school_id)
+
+#     def get(self, request, school_id):
+#         try:
+#             admissions = self.get_admissions_by_school(school_id)
+#             serializer = AdmissionSerializer(admissions, many=True)
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         except Exception as e:
+#             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
